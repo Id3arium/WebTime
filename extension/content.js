fce7ce510328a6b6d1f53057af859ada429edb1a
Alejandro <andres.al.campos@gmail.com>
2025-09-22 18:08:23 -0700

    cleanup

--- extension/content.js
+++ extension/content.js
@@ -1,14 +1,14 @@
 // This script creates a timer element and appends it to the body of the webpage.
 // It also provides functions to update the timer text and manage the timer state.
 
-let timerText = null; 
+let timerText = null;
 let lastActivityTime = Date.now();
 
 function createTimerElement() {
     const timer = document.createElement("div");
-    timer.className = "web-time-timer"; 
+    timer.className = "web-time-timer";
 
-    timerText = document.createElement("div"); 
+    timerText = document.createElement("div");
     timerText.className = "web-time-timer-text";
     timerText.textContent = "00:00:00";
 
@@ -19,13 +19,15 @@ function createTimerElement() {
 }
 
 function handleIncomingMessage(message, sender, sendResponse) {
-  if (message.type === "TIME_UPDATE") {
-    if (timerText) {
-      timerText.textContent = Utils.formatTimeWithSeconds(message.time);
-    } else {
-      console.error("Timer text element not found when trying to update time!");
+    if (message.type === "TIME_UPDATE") {
+        if (timerText) {
+            timerText.textContent = Utils.formatTimeWithSeconds(message.time);
+        } else {
+            console.error(
+                "Timer text element not found when trying to update time!"
+            );
+        }
     }
-  }
 }
 
 function updateActivityState() {
@@ -34,19 +36,19 @@ function updateActivityState() {
 }
 
 // Set up event listeners for user activity
-document.addEventListener('scroll', updateActivityState);
-document.addEventListener('keydown', updateActivityState);
-document.addEventListener('mousemove', updateActivityState);
+document.addEventListener("scroll", updateActivityState);
+document.addEventListener("keydown", updateActivityState);
+document.addEventListener("mousemove", updateActivityState);
 
 function init() {
     console.log("initTimer()");
-    
+
     createTimerElement();
     browser.runtime.onMessage.addListener(handleIncomingMessage);
 
     try {
         const readyMessage = { type: "CONTENT_SCRIPT_READY" };
-        browser.runtime.sendMessage(readyMessage); 
+        browser.runtime.sendMessage(readyMessage);
     } catch (error) {
         console.error("Error sending CONTENT_SCRIPT_READY message:", error);
     }
